e2d9afed2278f9f1cb46d7558d02ba68bab47b4d
Swall0w <technext.jpn@gmail.com>
2017-06-15 13:33:06 +0900

    [add]:Add a plot file.

---
+++ plot.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['font.family'] = 'TakaoPGothic'
+
+def plot_dataframe(dataframe,cycle_num):
+    dataframe = dataframe[dataframe['DataSetting.サイクル回数:INT']==cycle_num]
+    del_columns = ['Index','Date','ClockTime','RawTime','TraceSampID','DataSetting.サイクル回数:INT']
+    dataframe = dataframe.drop(del_columns,axis=1)
+    dataframe.reset_index(inplace=True,drop=True)
+
+    dataframe.plot(subplots=True,figsize=(10,8))
+    filename = 'cycle_{0}.png'
+    plt.savefig(filename.format(cycle_num))
+
+def main():
+    dataframe = pd.read_csv('output.csv')
+    cycle_list = dataframe['DataSetting.サイクル回数:INT'].value_counts().index.tolist()
+    for cycle_num in cycle_list:
+        plot_dataframe(dataframe,cycle_num)
+
+if __name__ == '__main__':
+    main()
